a4f9d4c618569e8b77789a7fe47522bff85d54d6
Gabrielle <102875018+Gglasgow09@users.noreply.github.com>
2024-02-08 11:09:44 -0500

    added in analysis by creating a local db

---
+++ config.py
@@ -0,0 +1,11 @@
+aws_access_key_id = 'AKIAZI2LEECWUPJBXVVY'
+aws_secret_access_key = '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'
+region_name = 'us-east-1'
+bucket_name = 'cardiovascular-disease-data'
+
+rds_endpoint = 'cardio-db.crkc44qquibr.us-east-1.rds.amazonaws.com'
+rds_port = 3306
+rds_username = 'admin'
+rds_password = 'rootroot'
+rds_database = 'cardio-db'
+
